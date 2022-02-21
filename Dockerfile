36ecd23833170f6ab8909e85488f5e09775d278f
Ryan Sinnet <rsinnet@misorobotics.com>
2022-02-21 18:30:25 +0000

    Add Dockerfile and entrypoint.sh
    Add initial working version of builder.

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+# syntax=docker/dockerfile:1.3
+ARG ALPINE_VERSION=3.15
+FROM alpine:3.15
+WORKDIR /
+ARG BUILDX_VERSION=0.7.0
+ARG DOCKER_VERSION=20.10.11-r0
+ARG GCR_CRED_VERSION=2.1.0
+
+RUN echo "http://dl-cdn.alpinelinux.org/alpine/v3.15/community" >> /etc/apk/repositories \
+    && apk update -U --no-cache && apk add --no-cache bind-tools curl openssh docker=$DOCKER_VERSION \
+    && curl -fSsLo /usr/bin/buildx https://github.com/docker/buildx/releases/download/v${BUILDX_VERSION}/buildx-v${BUILDX_VERSION}.linux-amd64 \
+    && curl -fSsL https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${GCR_CRED_VERSION}/docker-credential-gcr_linux_amd64-${GCR_CRED_VERSION}.tar.gz | tar xzp -C /usr/bin docker-credential-gcr \
+    && docker-credential-gcr configure-docker --registries asia-docker.pkg.dev,eu-docker.pkg.dev,us-docker.pkg.dev,gcr.io,asia.gcr.io,eu.gcr.io,us.gcr.io \
+    && chmod a+x /usr/bin/buildx \
+    && rm -rf /lib/apk/db/scripts.tar \
+    && rm -r /var/cache/apk
+
+COPY ./entrypoint.sh /
+RUN chmod +x entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
+LABEL maintainer="Ryan Sinnet" \
+    github.account="https://github.com/MisoRobotics" \
+    dockerfile.github.page="https://github.com/MisoRobotics/cloudbuildx/blob/main/Dockerfile" \
+    description="Build multiarch containers on Google Cloud Build with Moby BuildKit, Docker Buildx, and QEMU." \
+    version="0.1.0"
+
+STOPSIGNAL SIGTERM
---
+++ entrypoint.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env sh
+
+# Copyright (c) 2022 Miso Robotics, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# This script is intended for use on Google Cloud Build.
+network=cloudbuild
+binfmt_version=v0.8
+buildkit_version=v0.9.3
+
+run_args="--privileged"
+driver_opts="image=moby/buildkit:${buildkit_version}"
+
+if [ -n "${network}" ]; then
+	run_args="${run_args} --network=${network}"
+	driver_opts="${driver_opts},network=${network}"
+fi
+
+echo "Running user mode emulation of selected binfmt(s) with QEMU."
+docker run ${run_args} "linuxkit/binfmt:${binfmt_version}"
+docker run ${run_args} --rm multiarch/qemu-user-static --reset -p yes
+
+echo "Creating BuildKit builder on ${network} network."
+export DOCKER_BUILDKIT=1 DOCKER_CLI_EXPERIMENTAL=enabled
+buildx create --use --name=mybuilder --driver-opt="${driver_opts}" \
+	--buildkitd-flags '--allow-insecure-entitlement network.host'
+buildx inspect --bootstrap
+
+# Buildkit creates the builder on the cloudbuild network, so use host-mode
+# networking to reuse the network stack of the builder container.
+# When invoking build, explicitly pass the address of the GCE Metadata service
+# because otherwise it ends up with the wrong address which does resolve but
+# does not authenticate properly.
+metadata_host=metadata.google.internal
+metadata_ip="$(dig +short "${metadata_host}")"
+
+echo "Invoking docker build with host entry ${metadata_host}:${metadata_ip}"
+buildx build \
+	--add-host "${metadata_host}:${metadata_ip}" \
+	--allow=network.host --network=host "$@"
